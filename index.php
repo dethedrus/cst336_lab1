733604e247727a84bde2c0cff19789684245d903
EC2 Default User <ec2-user@ip-172-31-6-226.us-west-1.compute.internal>
2020-11-01 05:10:59 +0000

    adding all pages

--- index.php
+++ index.php
@@ -1,9 +1,36 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> </title>
+        <title>Lab 1: My Personal Page Thingy</title>
+        <meta charset="utf-8">
     </head>
+    
     <body>
-        <h1>Welcome to my website!</h1>
+        <header>
+            <h1>David Szabo</h1>
+        </header>
+
+        <hr>
+        
+        <nav>
+            <a href="index.html">Home</a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+        
+        <main>
+            <figure>
+                <img src="images/not-a-cat.png">
+            </figure>
+            
+            <div>
+                Welcome to my site!
+            </div>
+        </main>
+        
+        <footer>
+            
+        </footer>
+        
     </body>
 </html>
