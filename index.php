95ca0216ead865e6787888dc8b1d5f73fa93f3ff
EC2 Default User <ec2-user@ip-172-31-6-226.us-west-1.compute.internal>
2020-11-01 06:14:57 +0000

    adding all pages

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     <head>
         <title>Lab 1: My Personal Page Thingy</title>
         <meta charset="utf-8">
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
     </head>
     
     <body>
@@ -13,7 +15,7 @@
         <hr>
         
         <nav>
-            <a href="index.html">Home</a>
+            <a href="index.html" style="color:#FF0000;"><strong>Home</strong></a>
             <a href="about.html">About</a>
             <a href="contact.html">Contact</a>
         </nav>
@@ -24,12 +26,20 @@
             </figure>
             
             <div>
-                Welcome to my site!
+                <h3>Welcome to my site!</h3>
+                <p>What were you expecting?</p>
+                <p>Clean, well lit shots of a professional software architect?</p>
+                <p>Or perhaps just a little silliness in the form of what is best described as "<em>not a cat</em>"?</p>
             </div>
         </main>
         
+        <hr>
+        
         <footer>
-            
+            <img src="images/csumb-logo.png">
+            <div>CST 336: Internet Programming - 2020 &copy; Szabo<br /><br />
+            <strong>Disclaimer</strong>: <em>The information in this webpage is<br />
+            complete nonsense and used for academic purposes only.</em></div>
         </footer>
         
     </body>
